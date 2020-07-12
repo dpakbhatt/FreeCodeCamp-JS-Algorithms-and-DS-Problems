0e75fcaa87e034ed1f6230d7da866031560ad3af
Deepak Bhatt <32162875+dpakbhatt@users.noreply.github.com>
2020-07-13 03:45:51 +0530

    Problem2

--- Cash Register.js
+++ Cash Register.js
@@ -1,4 +1,3 @@
-// Create an array of objects which hold the denominations and their values
 var denom = [
   { name: "ONE HUNDRED", val: 100.0 },
   { name: "TWENTY", val: 20.0 },
@@ -15,7 +14,6 @@ function checkCashRegister(price, cash, cid) {
   var output = { status: null, change: [] };
   var change = cash - price;
 
-  // Transform CID array into drawer object
   var register = cid.reduce(
     function(acc, curr) {
       acc.total += curr[1];
@@ -25,53 +23,41 @@ function checkCashRegister(price, cash, cid) {
     { total: 0 }
   );
 
-  // Handle exact change
   if (register.total === change) {
     output.status = "CLOSED";
     output.change = cid;
     return output;
   }
 
-  // Handle obvious insufficient funds
   if (register.total < change) {
     output.status = "INSUFFICIENT_FUNDS";
     return output;
   }
-
-  // Loop through the denomination array
   var change_arr = denom.reduce(function(acc, curr) {
     var value = 0;
-    // While there is still money of this type in the drawer
-    // And while the denomination is larger than the change remaining
     while (register[curr.name] > 0 && change >= curr.val) {
       change -= curr.val;
       register[curr.name] -= curr.val;
       value += curr.val;
-
-      // Round change to the nearest hundreth deals with precision errors
       change = Math.round(change * 100) / 100;
     }
-    // Add this denomination to the output only if any was used.
+    
     if (value > 0) {
       acc.push([curr.name, value]);
     }
-    return acc; // Return the current change_arr
-  }, []); // Initial value of empty array for reduce
-
-  // If there are no elements in change_arr or we have leftover change, return
-  // the string "Insufficient Funds"
+    return acc; 
+  }, []); 
+  
   if (change_arr.length < 1 || change > 0) {
     output.status = "INSUFFICIENT_FUNDS";
     return output;
   }
-
-  // Here is your change, ma'am.
+  
   output.status = "OPEN";
   output.change = change_arr;
   return output;
 }
 
-// test here
 checkCashRegister(19.5, 20.0, [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
